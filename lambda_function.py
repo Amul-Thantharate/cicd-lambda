09206fc82845912390010e5918275176659f9453
Amul Thantharate <amulthantharate@gmail.com>
2023-10-25 12:59:16 +0530

    Changes in Lambda Function

--- lambda_function.py
+++ lambda_function.py
@@ -2,11 +2,11 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
-    print("event: ", event)
-    print("context: ", context)
+    name = event['name']
+    age = event['age']
+    print("Name: " + name)
+    print("Age: " + str(age))
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': f'Hello {name}! You are {age} years old.'
     }
-
